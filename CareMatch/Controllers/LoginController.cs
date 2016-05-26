114693943cf2b69d84083dcb5618a6bb2374904e
Mark van Lierop <mark.vanlierop@student.fontys.nl>
2016-05-26 10:48:43 +0200

    Login werkt.

--- CareMatch/Controllers/LoginController.cs
+++ CareMatch/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CAREMATCH;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,31 @@ namespace CareMatch.Controllers
         // GET: Login
         public ActionResult Index(string gebruikersnaam, string wachtwoord)
         {
+            Database database = new Database();
+
+            if (string.IsNullOrEmpty(gebruikersnaam) && string.IsNullOrEmpty(wachtwoord))
+            {
+                ViewBag.foutmelding = "";
+            }
+            else if(database.GebruikerLogin(gebruikersnaam, wachtwoord) == null)
+            {
+                ViewBag.foutmelding = "Gebruikersnaam of Wachtwoord is incorrect";
+            }
+            else if(database.GebruikerLogin(gebruikersnaam, wachtwoord).Rol.ToLower() == "beheerder")
+            {
+                Session["Gebruiker"] = database.GebruikerLogin(gebruikersnaam, wachtwoord);
+                return RedirectToAction("Index", "Beheerder", new { area = "" });
+            }
+            else if(database.GebruikerLogin(gebruikersnaam, wachtwoord).Rol.ToLower() == "vrijwilliger")
+            {
+                Session["Gebruiker"] = database.GebruikerLogin(gebruikersnaam, wachtwoord);
+                return RedirectToAction("Index", "Vrijwilliger", new { area = "" });
+            }
+            else if(database.GebruikerLogin(gebruikersnaam, wachtwoord).Rol.ToLower() == "hulpbehoevende")
+            {
+                Session["Gebruiker"] = database.GebruikerLogin(gebruikersnaam, wachtwoord);
+                return RedirectToAction("Index", "Hulpbehoevende", new { area = "" });
+            }
             return View();
         }
     }
