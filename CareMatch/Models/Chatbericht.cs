766f3f6809221198c01d9f44faff0482208050ed
Mark van Lierop <mark.vanlierop@student.fontys.nl>
2016-05-26 10:07:58 +0200

    Classes imported

---
+++ CareMatch/Models/Chatbericht.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace CAREMATCH
+{
+    class Chatbericht
+    {
+        //Properties
+        public int ID { get; }
+        public string Inhoud { get; }
+        public string VerzenderNaam { get; }
+        public DateTime datumtijd { get; }
+
+        //Properties
+
+
+        //Constructor
+        public Chatbericht(string inhoud, string verzender, int id, DateTime datum)
+        {
+            Inhoud = inhoud.Replace(System.Environment.NewLine, "");
+            VerzenderNaam = verzender;
+            ID = id;
+            datumtijd = datum;
+        }
+
+        public override string ToString()
+        {
+            return VerzenderNaam + ": " + Inhoud;
+        }
+    }
+}
