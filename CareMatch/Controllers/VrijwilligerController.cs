001255553194c6342ac18e38d407b54127cbc861
Joram Tetelepta <joramspam@hotmail.com>
2016-06-16 11:34:52 +0200

    Controllers zijn geStyleCopintatord

--- CareMatch/Controllers/VrijwilligerController.cs
+++ CareMatch/Controllers/VrijwilligerController.cs
@@ -30,8 +30,10 @@ namespace CareMatch.Controllers
                 agenda.AfspraakDatum = datum;
                 agenda.DatumTijdEind = tot;
                 agenda.DatumTijdStart = van;
-                //carematch.database.AgendaPuntToevoegen();
+
+                // carematch.database.AgendaPuntToevoegen();
             }
+
             return View();
         }
 
@@ -39,9 +41,10 @@ namespace CareMatch.Controllers
         {
             return View();
         }
+
         public ActionResult Profiel(string wachtwoord, string pasfoto, string info, bool? auto)
         {
-            if (Request.Form.Count >0)
+            if (Request.Form.Count > 0)
             {
                 if (!string.IsNullOrEmpty(wachtwoord))
                 {
@@ -50,6 +53,7 @@ namespace CareMatch.Controllers
                     ((Gebruiker)Session["Gebruiker"]).GebruikerInfo = info;
                     carematch.database.GebruikerProfielAanpassen(Session["Gebruiker"] as Gebruiker, true, false);
                 }
+            
                 if (!string.IsNullOrEmpty(pasfoto))
                 {
                     ((Gebruiker)Session["Gebruiker"]).Pasfoto = pasfoto;
@@ -64,19 +68,22 @@ namespace CareMatch.Controllers
                     carematch.database.GebruikerProfielAanpassen(Session["Gebruiker"] as Gebruiker, false, false);
                 }
             }
+
             return View();
         }
+
         public ActionResult HulpvragenOverzicht()
         {
             Gebruiker gebruiker = Session["Gebruiker"] as Gebruiker;
             ViewData["hulpvragen"] = carematch.database.HulpvragenOverzicht(gebruiker, Request.QueryString["filter"]);
             return View();
         }
+
         public ActionResult Hulpvraag(int id)
         {
             Gebruiker gebruiker = Session["Gebruiker"] as Gebruiker;
             Hulpvraag selectedhulpvraag = null;
-            List<Hulpvraag> hulpvragen = carematch.database.HulpvragenOverzicht(gebruiker, "");
+            List<Hulpvraag> hulpvragen = carematch.database.HulpvragenOverzicht(gebruiker, string.Empty);
             foreach (Hulpvraag hulpvraag in hulpvragen)
             {
                 if (hulpvraag.HulpvraagID == id)
@@ -84,9 +91,11 @@ namespace CareMatch.Controllers
                     selectedhulpvraag = hulpvraag;
                 }
             }
+
             ViewData["Hulpvraag"] = selectedhulpvraag;
             return View();
         }
+
         public ActionResult HulpvraagAannemen(int id)
         {
             Gebruiker gebruiker = Session["Gebruiker"] as Gebruiker;
@@ -110,11 +119,11 @@ namespace CareMatch.Controllers
                 return View();
             }
         }
+
         [HttpPost]
         public ActionResult ChatBarcode(string partner)
         {
-            return RedirectToAction("ChatBekijken", "Chat", new { partner = partner});
+            return RedirectToAction("ChatBekijken", "Chat", new { partner = partner });
         }
     }
 }
-
